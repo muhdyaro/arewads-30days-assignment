1dfd7716928b0bde5af527ee894d9f12c70cb046
Muhammad Yaro <muhdayaro@gmail.com>
2023-12-07 22:05:41 +0100

    day 1 & day 2 assignments

---
+++ day1/hello.py
@@ -0,0 +1,26 @@
+# Introduction
+# Day 1 - 30DaysOfPython Challenge
+
+print(2 + 3)   # addition(+)
+print(3 - 1)   # subtraction(-)
+print(2 * 3)   # multiplication(*)
+print(3 / 2)   # division(/)
+print(3 ** 2)  # exponential(**)
+print(3 % 2)   # modulus(%)
+print(3 // 2)  # Floor division operator(//)
+
+#Checking String
+print('Muhammd Abubakar Yaro')
+print('Yaro')
+print('Nigeria')
+print('I am enjoying 30 days of pyhton')
+# Checking data types
+
+print(type(10))                  # Int
+print(type(3.14))                # Float
+print(type(1 + 3j))                     # Complex
+print(type('Mhammad', 'Abubakar', 'Finland'))          # String
+print(type('Muhammad')) 
+print(type('Yaros'))
+print(type('Nigeria'))
+
---
+++ helloworld.py
@@ -0,0 +1,8 @@
+#Day 1 30DaysOfPython
+print(2 + 3)
+print(3 - 1)
+print(2 * 3)
+print(3 / 2)
+print(3 ** 2)
+print(3 % 2)
+print(3 // 2)
